efd190766e12ccb524759ca41da301f9676ea75d
Alex <alex.l@shopperplus.com>
2019-09-25 14:23:21 +0800

    update

---
+++ javascripts/index.js
@@ -0,0 +1,9 @@
+$(function(){
+
+	$(".carousel-control-prev").click(function(){
+	  $("#carousel").carousel("prev");
+	});
+	$(".carousel-control-next").click(function(){
+	  $("#carousel").carousel("next");
+	});
+})
