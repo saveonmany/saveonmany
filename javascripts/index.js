69aa951e7124d31eaa0325636d396ed5f5e4388c
Alex <alex.l@shopperplus.com>
2019-09-27 14:06:52 +0800

    update

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,5 +1,19 @@
 $(function(){
+	//修改dom结构
+	$('body').addClass('saveonmany')
+	$('.sub-nav').prepend($('.lev1'))
+	$('.indexTabBox').append($('#CentralArea'))
+	$('.indexTabRight').append($('.v4topleft>table, .v4topright>table'))
+	$('.indexTabRight').append($('#LeftPanel'))
 
+	//sub-nav display
+	$('.menu-category').hover(function(){
+    $('.sub-nav').slideDown()
+    },function(){
+    $('.sub-nav').slideUp()
+  });
+
+	//banner carousel
 	$(".carousel-control-prev").click(function(){
 	  $("#carousel").carousel("prev");
 	});
